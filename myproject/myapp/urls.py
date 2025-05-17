22d8c5d94917eab73055b775205f921d49d0d78b
Kanokpond-A <158846727+Kanokpond-A@users.noreply.github.com>
2025-05-17 18:01:45 +0700

    update readme.md

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),    
     path('trees/<int:pk>/', views.tree_list, name='tree_list'),
     path('trees-equipment/', views.tree_equipment_list, name='tree_equipment_list'),
+    path('cart/', views.cart, name='cart'),
+path('add-to-cart/<str:item_type>/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
+path('order/', views.order, name='order'),
+
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -21,5 +21,39 @@ def tree_equipment_list(request):
         'query': query,
     })
 
+def add_to_cart(request, item_type, item_id):
+    cart = request.session.get('cart', [])
+    cart.append({'type': item_type, 'id': item_id, 'quantity': 1})
+    request.session['cart'] = cart
+    return redirect('cart')
+
+def cart(request):
+    cart_items = request.session.get('cart', [])
+    display_items = []
+
+    for item in cart_items:
+        if item['type'] == 'tree':
+            obj = Tree.objects.get(id=item['id'])
+        elif item['type'] == 'equipment':
+            obj = Equipment.objects.get(id=item['id'])
+        else:
+            continue
+
+        display_items.append({
+            'type': item['type'],
+            'id': item['id'],
+            'name': obj.name,
+            'image_url': obj.image_url,
+            'price': obj.price,
+            'quantity': item.get('quantity', 1),
+        })
+
+    total_price = sum([i['price'] * i['quantity'] for i in display_items])
+
+    return render(request, 'cart.html', {
+        'items': display_items,
+        'total': total_price,
+    })
+
 def tree_list(request):
     return render(request, 'tree_list.html')
